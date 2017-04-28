2d11e63e8769c99c827ef7ba33b003428f441887
sunpun <fed@sunpun.com>
2017-04-28 19:07:40 +0800

    Include PHP CMS.

---
+++ data/config/config.ini.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'site_name' => 'XiaoCms企业建站版',
+  'site_theme' => 'default',
+  'site_mobile' => '0',
+  'wap' => 'm',
+  'site_title' => 'XiaoCms演示站',
+  'site_keywords' => 'xiaocms',
+  'site_description' => '欢迎使用xiaocms内容管理系统 官方网站：http://www.xiaocms.com',
+  'site_status' => '1|正常
+2|头条
+3|推荐
+0|未审核',
+  'site_download_image' => '0',
+  'site_watermark' => '0',
+  'site_watermark_pos' => '5',
+  'member_modelid' => '5',
+  'qq_login' => '0',
+  'appid' => '',
+  'appkey' => '',
+  'member_register' => '0',
+  'member_status' => '1',
+  'member_regcode' => '1',
+  'member_logincode' => '1',
+  'diy_url' => '0',
+  'list_url' => '{catdir}/',
+  'list_page_url' => '{catdir}/{page}',
+  'show_url' => '{catdir}/{id}.html',
+  'show_page_url' => '{catdir}/{id}_{page}.html',
+  'smtpserver' => 'smtp.163.com',
+  'smtpserverport' => '25',
+  'smtpusermail' => '',
+  'smtpuser' => 'admin',
+  'smtppass' => 'admin',
+  'smtpemailto' => '',
+  'rand_code' => 'b956d782f8f56e7fbcb9b3efb27a5668',
+);
